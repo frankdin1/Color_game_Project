84831117ca1d6298ca2e983834835e01d37f0dec
frankdin <frankdin@yahoo.com>
2020-10-12 14:48:12 -0500

    added toggle functionality to hard and easy mode buttons

--- Color_Game_Project/RGB.js
+++ Color_Game_Project/RGB.js
@@ -3,6 +3,19 @@ var colorDisplay = document.querySelector("#colorDisplay");
 var resultDisplay = document.querySelector("#resultDisplay");
 var newColors = document.querySelector("#newColors");
 var titleBar1 = document.querySelector("#title-bar-1");
+var easyMode = document.querySelector("#easy");
+var hardMode = document.querySelector("#hard");
+
+easyMode.addEventListener("click", function(){
+	hard.classList.remove("selected");
+	easy.classList.add("selected");
+});
+
+hardMode.addEventListener("click", function(){
+	easy.classList.remove("selected");
+	hard.classList.add("selected");
+});
+
 var rgb = [];
 function randomColor(){
 	var red = [];
@@ -25,42 +38,20 @@ function assignColors(){
 }
 
 var pickedColor = randomColor();
-console.log("Initial value of pickedColor: "+pickedColor);
+titleBar1.style.backgroundColor = "blue";
+//console.log("Initial value of pickedColor: "+pickedColor);
 
 newColors.addEventListener("click", function(){
 	newColors.innerHTML = "New Colors";
-	titleBar1.style.backgroundColor = "#232323";
+	titleBar1.style.backgroundColor = "blue";
 	var pickedColor = randomColor();
-	console.log("value of picedColor inside reset event listener: "+pickedColor);
+	//console.log("value of picedColor inside reset event listener: "+pickedColor);
 	colorDisplay.innerHTML = pickedColor
 	assignColors();
-
-
-// 	for (var i = 0; i < squares.length; i++){
-// 	squares[i].style.backgroundColor = rgb[i];
-// 	squares[i].addEventListener("click", function(){
-// 		var clickedColor = this.style.backgroundColor;
-// 		if (clickedColor === pickedColor){
-// 			resultDisplay.innerHTML = "Correct";
-// 			for (var i = 0; i < squares.length; i++){
-// 			squares[i].style.backgroundColor = pickedColor;
-// 			}
-// 			newColors.innerHTML = "PLAY AGAIN?";
-// 			titleBar1.style.backgroundColor = pickedColor;
-// 		}
-// 		else{
-// 			resultDisplay.innerHTML = "Try Again!!";
-// 			this.style.backgroundColor = "#232323";
-// 		}
-
-// 	})
-// }
-
-});	
-
+});		
 
 colorDisplay.innerHTML = pickedColor;
-console.log("third value of pickedColor: "+pickedColor);
+//console.log("third value of pickedColor: "+pickedColor);
 
 //console.log(pickedColor)
 for (var i = 0; i < squares.length; i++){
