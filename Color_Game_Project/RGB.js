639dd1a46f8946421ccc6c1d73ab8e8aab957ec3
frankdin <frankdin@yahoo.com>
2020-10-12 13:49:57 -0500

    added reset button to assign new random colors and reset game

--- Color_Game_Project/RGB.js
+++ Color_Game_Project/RGB.js
@@ -1,6 +1,8 @@
 var squares = document.querySelectorAll(".square");
-var colorDisplay = document.querySelector("#colorDisplay")
-var resultDisplay = document.querySelector("#resultDisplay")
+var colorDisplay = document.querySelector("#colorDisplay");
+var resultDisplay = document.querySelector("#resultDisplay");
+var newColors = document.querySelector("#newColors");
+var titleBar1 = document.querySelector("#title-bar-1");
 var rgb = [];
 function randomColor(){
 	var red = [];
@@ -16,8 +18,50 @@ function randomColor(){
 	return rgb[Math.floor(Math.random()*squares.length)];
 }
 
+function assignColors(){
+	for (var i = 0; i < squares.length; i++){
+	squares[i].style.backgroundColor = rgb[i];
+	}
+}
+
 var pickedColor = randomColor();
-colorDisplay.innerHTML = pickedColor
+console.log("Initial value of pickedColor: "+pickedColor);
+
+newColors.addEventListener("click", function(){
+	newColors.innerHTML = "New Colors";
+	titleBar1.style.backgroundColor = "#232323";
+	var pickedColor = randomColor();
+	console.log("value of picedColor inside reset event listener: "+pickedColor);
+	colorDisplay.innerHTML = pickedColor
+	assignColors();
+
+
+// 	for (var i = 0; i < squares.length; i++){
+// 	squares[i].style.backgroundColor = rgb[i];
+// 	squares[i].addEventListener("click", function(){
+// 		var clickedColor = this.style.backgroundColor;
+// 		if (clickedColor === pickedColor){
+// 			resultDisplay.innerHTML = "Correct";
+// 			for (var i = 0; i < squares.length; i++){
+// 			squares[i].style.backgroundColor = pickedColor;
+// 			}
+// 			newColors.innerHTML = "PLAY AGAIN?";
+// 			titleBar1.style.backgroundColor = pickedColor;
+// 		}
+// 		else{
+// 			resultDisplay.innerHTML = "Try Again!!";
+// 			this.style.backgroundColor = "#232323";
+// 		}
+
+// 	})
+// }
+
+});	
+
+
+colorDisplay.innerHTML = pickedColor;
+console.log("third value of pickedColor: "+pickedColor);
+
 //console.log(pickedColor)
 for (var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = rgb[i];
@@ -28,12 +72,13 @@ for (var i = 0; i < squares.length; i++){
 			for (var i = 0; i < squares.length; i++){
 			squares[i].style.backgroundColor = pickedColor;
 			}
-
+			newColors.innerHTML = "PLAY AGAIN?";
+			titleBar1.style.backgroundColor = pickedColor;
 		}
 		else{
 			resultDisplay.innerHTML = "Try Again!!";
 			this.style.backgroundColor = "#232323";
-		}	
+		}
+
 	})
 }
-
