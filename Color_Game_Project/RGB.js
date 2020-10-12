6a5a3a031430aa714c3714f85fecca3efda58d06
frankdin <frankdin@yahoo.com>
2020-10-12 15:54:51 -0500

    modified my color generator function;added functionality to easy/hard mode buttons

--- Color_Game_Project/RGB.js
+++ Color_Game_Project/RGB.js
@@ -5,49 +5,77 @@ var newColors = document.querySelector("#newColors");
 var titleBar1 = document.querySelector("#title-bar-1");
 var easyMode = document.querySelector("#easy");
 var hardMode = document.querySelector("#hard");
+var colors = randomColorArr(6)
+hard.classList.add("selected");
 
 easyMode.addEventListener("click", function(){
 	hard.classList.remove("selected");
 	easy.classList.add("selected");
+	colors = randomColorArr(3);
+	pickedColor = pickColor();
+	colorDisplay.innerHTML = pickedColor;
+	for (var i = 0; i < squares.length; i++){
+		if (colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		}
+		else{
+			squares[i].style.display = "none";
+		}
+	}
 });
 
 hardMode.addEventListener("click", function(){
 	easy.classList.remove("selected");
 	hard.classList.add("selected");
+	colors = randomColorArr(6);
+	pickedColor = pickColor();
+	colorDisplay.innerHTML = pickedColor;
+	for (var i = 0; i < squares.length; i++){
+		squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		}
 });
 
 var rgb = [];
+
+function randomColorArr(num) {
+	//make an array
+	var arr = [];
+	//add num random colors to arr
+	for(var i = 0; i < num; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
 function randomColor(){
-	var red = [];
-	var green = [];
-	var blue = [];
-	
-	for (var i = 0; i < squares.length; i++){
-		red[i] = Math.floor(Math.random()*255)
-		green[i] = Math.floor(Math.random()*256)
-		blue[i] = Math.floor(Math.random()*256)
-		rgb[i] = "rgb(" + red[i] + ", " + green[i] + ", " + blue[i] + ")";
-	}
-	return rgb[Math.floor(Math.random()*squares.length)];
+	var red = Math.floor(Math.random()*256)
+	var green = Math.floor(Math.random()*256)
+	var blue = Math.floor(Math.random()*256)
+	return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
-function assignColors(){
-	for (var i = 0; i < squares.length; i++){
-	squares[i].style.backgroundColor = rgb[i];
-	}
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
 
-var pickedColor = randomColor();
+var pickedColor = pickColor();
 titleBar1.style.backgroundColor = "blue";
 //console.log("Initial value of pickedColor: "+pickedColor);
 
 newColors.addEventListener("click", function(){
 	newColors.innerHTML = "New Colors";
 	titleBar1.style.backgroundColor = "blue";
-	var pickedColor = randomColor();
+	var pickedColor = pickColor();
+	var colors = randomColorArr(6);
 	//console.log("value of picedColor inside reset event listener: "+pickedColor);
 	colorDisplay.innerHTML = pickedColor
-	assignColors();
+	for (var i = 0; i < squares.length; i++){
+	squares[i].style.backgroundColor = colors[i];
+	}
 });		
 
 colorDisplay.innerHTML = pickedColor;
@@ -55,7 +83,7 @@ colorDisplay.innerHTML = pickedColor;
 
 //console.log(pickedColor)
 for (var i = 0; i < squares.length; i++){
-	squares[i].style.backgroundColor = rgb[i];
+	squares[i].style.backgroundColor = colors[i];
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.backgroundColor;
 		if (clickedColor === pickedColor){
