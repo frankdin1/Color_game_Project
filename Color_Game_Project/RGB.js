9eb3e9217a51ae2c44c468c47bb49948b70c78c8
frankdin <frankdin@yahoo.com>
2020-10-13 12:00:48 -0500

    refactored the js script some more

--- Color_Game_Project/RGB.js
+++ Color_Game_Project/RGB.js
@@ -1,6 +1,6 @@
 var numSquares = 3;
 var colors = randomColorArr(numSquares)
-
+var pickedColor;
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#colorDisplay");
 var resultDisplay = document.querySelector("#resultDisplay");
@@ -8,14 +8,26 @@ var resetBtn = document.querySelector("#reset");
 var h1 = document.querySelector("h1");
 var gameMode = document.querySelectorAll(".gameMode");
 
+init();
+
+function init(){
+	easyMode();
+	modeBtns();
+	gamePlay();
+	reset();
+}
+
 function reset(){
 	resetBtn.innerHTML = "New Colors";
 	resultDisplay.innerHTML = "";
 	h1.style.backgroundColor = "steelblue";
 	colors = randomColorArr(numSquares);
 	pickedColor = pickColor();
-	
-	colorDisplay.innerHTML = pickedColor;
+	colorDisplay.innerHTML = pickedColor;//this line displays the rgb that user will try to guess
+	//we use the reset button to start a new game instead of having to refresh the screen each time
+	resetBtn.addEventListener("click", function(){
+		reset();
+	});	
 	easyMode();
 }
 
@@ -57,57 +69,49 @@ function easyMode(){
 	}
 }
 
-easyMode();//calling easy mode function
-
 //this click event is what we use to change modes and style of mode buttons
-for(var i = 0; i < gameMode.length; i++){
-	gameMode[i].addEventListener("click", function(){
+function modeBtns(){
 		for(var i = 0; i < gameMode.length; i++){
-			gameMode[i].classList.remove("selected");//initially remove the "selected" class from all elements in the .gameMode class
-			this.classList.add("selected");//add the "selected" class to the element that was clicked
-		}
+		gameMode[i].addEventListener("click", function(){
+			for(var i = 0; i < gameMode.length; i++){
+				gameMode[i].classList.remove("selected");//initially remove the "selected" class from all elements in the .gameMode class
+				this.classList.add("selected");//add the "selected" class to the element that was clicked
+			}
 
-		//the following statements determine how many squares will be used as parameters for the color creating function
-		if (this.textContent === "Easy"){
-			numSquares = 3;
-		}
-		else if(this.textContent === "Medium"){
-			numSquares = 6;
-		}
-		else{
-			numSquares = 9;
-		}
-		reset();
-		
-	})
+			//the following statements determine how many squares will be used as parameters for the color creating function
+			if (this.textContent === "Easy"){
+				numSquares = 3;
+			}
+			else if(this.textContent === "Medium"){
+				numSquares = 6;
+			}
+			else{
+				numSquares = 9;
+			}
+			reset();
+			
+		})
+	}
 }
 
-var pickedColor = pickColor();
-
-//we use the reset button to start a new game instead of having to refresh the screen each time
-resetBtn.addEventListener("click", function(){
-	reset();
-});		
-
-colorDisplay.innerHTML = pickedColor;//this line displays the rgb that user will try to guess
-
-//the code below determines what happens when we select ofr fail to select the square with the right color
-for (var i = 0; i < squares.length; i++){
-	squares[i].style.backgroundColor = colors[i];
-	squares[i].addEventListener("click", function(){
-		var clickedColor = this.style.backgroundColor;
-		if (clickedColor === pickedColor){//if the square you clicked on has the same color as the color at the tp of the screen
-			resultDisplay.innerHTML = "Correct";//display this message
-			for (var i = 0; i < squares.length; i++){
-			squares[i].style.backgroundColor = pickedColor;//let all the sqaures change color to that of the "winning" color
+//the code below determines what happens when we select or fail to select the square with the right color
+function gamePlay(){
+	for (var i = 0; i < squares.length; i++){
+		squares[i].addEventListener("click", function(){
+			var clickedColor = this.style.backgroundColor;
+			if (clickedColor === pickedColor){//if the square you clicked on has the same color as the color at the tp of the screen
+				resultDisplay.innerHTML = "Correct";//display this message
+				for (var i = 0; i < squares.length; i++){
+				squares[i].style.backgroundColor = pickedColor;//let all the sqaures change color to that of the "winning" color
+				}
+				resetBtn.innerHTML = "PLAY AGAIN?";//change the mesage displayed
+				h1.style.backgroundColor = pickedColor;//change the title background color
 			}
-			resetBtn.innerHTML = "PLAY AGAIN?";//change the mesage displayed
-			h1.style.backgroundColor = pickedColor;//change the title background color
-		}
-		else{//if you click the wrong square
-			resultDisplay.innerHTML = "Try Again!!";//display this message
-			this.style.backgroundColor = "#232323";//make the square disappear
-		}
+			else{//if you click the wrong square
+				resultDisplay.innerHTML = "Try Again!!";//display this message
+				this.style.backgroundColor = "#232323";//make the square disappear
+			}
 
-	})
+		})
+	}
 }
